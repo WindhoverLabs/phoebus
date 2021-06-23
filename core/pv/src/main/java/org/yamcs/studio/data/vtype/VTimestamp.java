77b7fba5781089203e562ef17096571c6e276487
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-06-23 15:39:21 -0500

    -Removed dependency on fork

--- core/pv/src/main/java/org/yamcs/studio/data/vtype/VTimestamp.java
+++
@@ -1,9 +0,0 @@
-package org.yamcs.studio.data.vtype;
-
-import java.util.Date;
-
-public interface VTimestamp extends Scalar, Alarm, Time, Display, VType {
-
-    @Override
-    Date getValue();
-}
--- core/pv/src/main/java/org/yamcs/studio/data/vtype/VType.java
+++
@@ -1,7 +0,0 @@
-package org.yamcs.studio.data.vtype;
-
-/**
- * Tag interface to mark all the members of the value classes.
- */
-public interface VType {
-}
