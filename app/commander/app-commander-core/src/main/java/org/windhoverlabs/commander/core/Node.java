0d08f21c8aa77d9aaf9f3a1208d184389336fc39
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-10-27 11:24:26 -0500

    -Fix CI.

--- app/commander/app-commander-core/src/main/java/org/windhoverlabs/commander/core/Node.java
+++ app/commander/app-commander-core/src/main/java/org/windhoverlabs/commander/core/Node.java
@@ -1,5 +1,13 @@
 package org.windhoverlabs.commander.core;
 
-public abstract class Node {
-
+/**
+ * @author lgomez
+ *
+ *A stream is usually a namespace for data coming from some type of Connection.
+ *@apiNote Not sure if the Stream should just be a Node.
+ */
+public interface Stream {
+public void activate();
+public void deactivate();
+public StreamState getStreamState();
 }
