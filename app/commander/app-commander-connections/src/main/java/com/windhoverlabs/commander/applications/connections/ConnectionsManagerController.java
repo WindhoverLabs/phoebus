b2dddd1e53fbc0973b80d8c3bf064a5f1ed2b00d
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-11-10 17:41:30 -0600

    -Add context menu to Connections Manager.

--- app/commander/app-commander-connections/src/main/java/com/windhoverlabs/commander/applications/connections/ConnectionsManagerController.java
+++ app/commander/app-commander-connections/src/main/java/com/windhoverlabs/commander/applications/connections/ConnectionsManagerController.java
@@ -2,70 +2,73 @@ package com.windhoverlabs.commander.applications.connections;
 
 import static com.windhoverlabs.commander.applications.connections.ConnectionsManagerInstance.logger;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Level;
-import org.phoebus.framework.selection.SelectionService;
-import org.phoebus.framework.spi.AppResourceDescriptor;
-import org.phoebus.framework.util.ResourceParser;
-import org.phoebus.framework.workbench.ApplicationService;
-import org.phoebus.ui.application.ApplicationLauncherService;
-import org.phoebus.ui.application.ContextMenuService;
-import org.phoebus.ui.application.PhoebusApplication;
-import org.phoebus.ui.dialog.DialogHelper;
 import org.phoebus.ui.javafx.ImageCache;
 
-import javafx.application.Platform;
-import javafx.beans.InvalidationListener;
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TreeTableView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ContextMenuEvent;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
-import org.phoebus.ui.spi.ContextMenuEntry;
 
 /**
- * Controller for the commander app
+ * Controller for the Connections Manager App
  *
  * @lgomez
  */
 @SuppressWarnings("nls")
 public class ConnectionsManagerController {
-
-    public ConnectionsManagerController()
-    {
-    }
-
     @FXML
-    public void initialize() {
-    }
+    TreeTableView<Object> serverConnectionsTableView;
 
-    /** Call when no longer needed */
-    public void shutdown()
-    {
-       System.out.println("shutdown");
-    }
+	public ConnectionsManagerController() {
+	}
+
+	@FXML
+	public void initialize() {
+	}
+
+	Image addProperties = ImageCache.getImage(ConnectionsManagerApp.class, "/icons/server_connection.png");
+
+	@FXML
+	public void createContextMenu() {
+		
+		final ContextMenu contextMenu = new ContextMenu();
+		// Add property to channel
+		MenuItem addServerConnection = new MenuItem("Add Connection", new ImageView(addProperties));
+		addServerConnection.setOnAction(e -> {
+
+			// get the list of server connections?
+//            properties = getClient().getAllProperties();
+//            AddPropertyDialog dialog = new AddPropertyDialog(tableView, properties);
+//            Optional<Property> result = dialog.showAndWait();
+//            result.ifPresent(property -> {
+//                if (addPropertyJob != null) {
+//                    addPropertyJob.cancel();
+//                }
+//                List<String> channelNames = tableView.getSelectionModel().getSelectedItems().stream().map(ch -> {
+//                    return ch.getName();
+//                }).collect(Collectors.toList());
+//                AddProperty2ChannelsJob.submit(getClient(),
+//                        channelNames,
+//                        property,
+//                        (url, ex) -> ExceptionDetailsErrorDialog.openError("ChannelFinder Query Error", ex.getMessage(), ex));
+
+//            });
+//        });
+		}
+
+		);
+		
+		contextMenu.getItems().add(addServerConnection);
+		
+		serverConnectionsTableView.setContextMenu(contextMenu);
+
+	}
+
+	/** Call when no longer needed */
+	public void shutdown() {
+		System.out.println("shutdown");
+	}
 }
