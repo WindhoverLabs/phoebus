32482825867e7df1a4a0a0128a2c0838d21c93da
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-11-19 15:33:42 -0600

    -Add NodeType.

--- core/commander-core/src/main/java/com/windhoverlabs/commander/core/NodeContext.java
+++ core/commander-core/src/main/java/com/windhoverlabs/commander/core/NodeContext.java
@@ -3,14 +3,24 @@ package com.windhoverlabs.commander.core;
 import java.util.List;
 
 /**
- * A NodeContext is a link to the ground system in the outside world. A YAMCS server is an example of this.
+ * A NodeContext is a link to the ground system in the outside world. A YAMCS
+ * server is an example of this.
+ * 
  * @author lgomez
  *
  */
 public abstract class NodeContext<T extends Node> {
-protected List<T> nodes = null;
-protected List<CommanderPlugin> plugins;
+	enum NodeType {
+		YAMCS
+	};
+
+	protected List<T> nodes = null;
+	private NodeType type;
+	protected List<CommanderPlugin> plugins;
+
 // TODO: Not sure if these functions make sense for this interface.
-public abstract void connect();
-public abstract void disconnect();
+	public abstract void connect();
+
+	public abstract void disconnect();
+
 }
