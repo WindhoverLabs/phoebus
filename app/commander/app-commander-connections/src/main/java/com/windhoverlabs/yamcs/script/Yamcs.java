805e78e4783fb27cfdca8488ed0d921ee78ca3d5
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2021-11-30 18:26:15 -0600

    -Add commands with arguments.

--- app/commander/app-commander-connections/src/main/java/com/windhoverlabs/yamcs/script/Yamcs.java
+++ app/commander/app-commander-connections/src/main/java/com/windhoverlabs/yamcs/script/Yamcs.java
@@ -64,10 +64,32 @@ public class Yamcs {
 	/**
 	 * Sample use:
 	 *
-	 * Yamcs.issueCommand('/YSS/SIMULATOR/SWITCH_VOLTAGE_ON', {"voltage_num": 1});
+	 * Yamcs.issueCommand('sitl:yamcs-cfs/YSS/SIMULATOR/SWITCH_VOLTAGE_ON', {"voltage_num": 1});
 	 */
-	public static void issueCommand(String command, String server, String instance, Map<String, Object> args) {
-		ProcessorClient processor = YamcsPlugin.getProcessorClient();
+	public static void issueCommand(String command, Map<String, Object> args) {
+		
+		
+		if (command == null) {
+			return ;
+		}
+
+		command = command.trim();
+
+		String serverName = null;
+		String instanceName = null;
+		try {
+			//TODO: Clean this up a bit
+			serverName = command.split(":")[0];
+			instanceName = command.split(":")[1].split("/")[0];
+		} catch (Exception e) {
+			log.warning(
+					"Error on expression \"" + command + "\". Verify that the instance and server names exist.");
+			return ;
+		}
+
+		command = command.split(":")[1].substring(command.split(":")[1].indexOf('/'));
+		ProcessorClient processor = getInstance(serverName, instanceName).getYamcsProcessor();
+
 		if (processor == null) {
 			log.warning("No active processor");
 			return;
