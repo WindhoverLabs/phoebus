92402e62dbfa398b5a3fba2944c49e6da446c615
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2022-01-21 11:27:30 -0600

    -Format test sources.

--- core/commander-core/src/test/java/com/windhoverlabs/commander/core/HttpClientTest.java
+++ core/commander-core/src/test/java/com/windhoverlabs/commander/core/HttpClientTest.java
@@ -3,46 +3,46 @@ package com.windhoverlabs.commander.core;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import io.netty.handler.codec.http.HttpMethod;
 import java.net.ConnectException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Test;
 import org.yamcs.client.ClientException;
 import org.yamcs.client.base.HttpClient;
 
-import io.netty.handler.codec.http.HttpMethod;
-
 public class HttpClientTest extends AbstractIntegrationTest {
 
-    HttpClient client = new HttpClient();
+  HttpClient client = new HttpClient();
 
-    @Test
-    public void testConnectionRefused() throws Exception {
-        Throwable t = null;
-        try {
-            client.doAsyncRequest("http://localhost:32323/blaba", HttpMethod.GET, null).get(2,
-                    TimeUnit.SECONDS);
-        } catch (ExecutionException e) {
-            t = e.getCause();
-        }
-
-        assertNotNull(t);
-        assertTrue(t instanceof ConnectException);
+  @Test
+  public void testConnectionRefused() throws Exception {
+    Throwable t = null;
+    try {
+      client
+          .doAsyncRequest("http://localhost:32323/blaba", HttpMethod.GET, null)
+          .get(2, TimeUnit.SECONDS);
+    } catch (ExecutionException e) {
+      t = e.getCause();
     }
 
-    @Test
-    public void test404NotFound() throws Exception {
-        Throwable t = null;
-        try {
-            client.doAsyncRequest("http://localhost:9190/blaba", HttpMethod.GET, null)
-                    .get(2, TimeUnit.SECONDS);
-        } catch (ExecutionException e) {
-            t = e.getCause();
-        }
+    assertNotNull(t);
+    assertTrue(t instanceof ConnectException);
+  }
 
-        assertNotNull(t);
-        assertTrue(t instanceof ClientException);
-        assertTrue(t.getMessage().contains("404"));
+  @Test
+  public void test404NotFound() throws Exception {
+    Throwable t = null;
+    try {
+      client
+          .doAsyncRequest("http://localhost:9190/blaba", HttpMethod.GET, null)
+          .get(2, TimeUnit.SECONDS);
+    } catch (ExecutionException e) {
+      t = e.getCause();
     }
+
+    assertNotNull(t);
+    assertTrue(t instanceof ClientException);
+    assertTrue(t.getMessage().contains("404"));
+  }
 }
