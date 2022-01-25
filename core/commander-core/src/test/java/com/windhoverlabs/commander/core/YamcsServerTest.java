6b5058e54b117279311c0fc650e5950f329a30e6
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2022-01-25 13:13:09 -0600

    -Cleanup

--- core/commander-core/src/test/java/com/windhoverlabs/commander/core/YamcsServerTest.java
+++ core/commander-core/src/test/java/com/windhoverlabs/commander/core/YamcsServerTest.java
@@ -3,13 +3,9 @@ package com.windhoverlabs.commander.core;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class YamcsServerTest extends AbstractIntegrationTest {
-
-  @Rule public ExpectedException exceptionGrabber = ExpectedException.none();
 
   @Test
   public void testYamcsServerConnect() {
