e3be29822f0aa43dad1995c04def24dca9128e27
lorenzo-gomez-windhover <lgomez@windhoverlabs.com>
2022-01-25 17:41:45 -0600

    -Test instances when connecting to YamcsServer

--- core/commander-core/src/test/java/com/windhoverlabs/commander/core/YamcsServerTest.java
+++ core/commander-core/src/test/java/com/windhoverlabs/commander/core/YamcsServerTest.java
@@ -3,12 +3,15 @@ package com.windhoverlabs.commander.core;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import org.junit.Test;
 
 public class YamcsServerTest extends AbstractIntegrationTest {
 
   @Test
-  public void testYamcsServerConnect() {
+  public void testYamcsServerConnect() throws InterruptedException, ExecutionException {
     YamcsServer newServer = new YamcsServer("sitl");
     assertThat(newServer.getName(), equalTo("sitl"));
 
@@ -18,5 +21,25 @@ public class YamcsServerTest extends AbstractIntegrationTest {
     newServer.connect(newConnection);
 
     assertThat(newServer.getServerState(), equalTo(ConnectionState.CONNECTED));
+
+    CompletableFuture<String> future = new CompletableFuture<>();
+
+    ScheduledThreadPoolExecutor executorService = new ScheduledThreadPoolExecutor(1);
+    executorService.submit(
+        new Runnable() {
+          @Override
+          public void run() {
+            try {
+              // TODO:Not sure if this the best way to do this
+              Thread.sleep(100);
+              assertThat(newServer.getItems().size(), equalTo(1));
+              future.complete("Success");
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+        });
+
+    assertThat(future.get(), equalTo("Success"));
   }
 }
