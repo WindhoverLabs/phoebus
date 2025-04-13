e3afd3191454304434c15b11d0aef3a5c08d25b4
lgomez <lgomez@windhoverlabs.com>
2025-04-13 15:56:12 -0500

    -auto_connect. Should be made configurable.

--- app/commander/app-commander-connections/src/main/java/com/windhoverlabs/yamcs/applications/connections/Tree.java
+++ app/commander/app-commander-connections/src/main/java/com/windhoverlabs/yamcs/applications/connections/Tree.java
@@ -147,6 +147,15 @@ public class Tree {
     SeparatorMenuItem sep = new SeparatorMenuItem();
     MenuItem connectAllServers = new MenuItem(Messages.ConnectAll);
 
+//    FIXME:Should be made configurable
+    if (true) {
+        System.out.println("Connect here********************");
+        for (YamcsServer s : YamcsObjectManager.getRoot().getItems()) {
+          attemptToConnect(s);
+        }
+      }
+
+
     connectAllServers.setOnAction(
         e -> {
           TreeItem<YamcsObject<?>> selectedItem = treeView.getSelectionModel().getSelectedItem();
